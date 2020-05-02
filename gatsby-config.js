b22ef3646c64553588bc1769b4d585e2847cd4ab
Fabian Neugart <f.neugart@mytaxi.com>
2020-05-02 13:40:00 +0200

    added meta infos

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Fabian Neugart`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://neugartf.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `_ngt__`,
     },
   },
   plugins: [
